8689e09dcda6a6e18d044c2e42b5204f2618892e
arjunkumar811 <kumararjun26401@gmail.com>
2025-07-01 14:04:18 +0530

    API Call

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,23 @@
 require("dotenv").config();
 
-console.log(process.env.CLAUDE_API_KEY)
+
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic();
+
+async function main() {
+    const msg = await anthropic.messages.create({
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 1000,
+        temperature: 1,
+        message: [{
+            role: "user", 
+            constent: "What is API"
+        }]
+    });
+    console.log(msg)
+}
+
+main()
+
+//console.log(process.env.CLAUDE_API_KEY)
