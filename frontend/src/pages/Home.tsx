43569d4af59568b6e91f9434a5cd2949f56baf44
arjunkumar811 <kumararjun26401@gmail.com>
2025-07-03 16:29:00 +0530

    Added HomePage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Wand2 } from 'lucide-react';
-import axios from "axios";
-
 
 export function Home() {
   const [prompt, setPrompt] = useState('');
